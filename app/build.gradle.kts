c07322741700ce0d23f294adaa89bb33b8989aa8
commits <adaryus@mail.com>
2026-04-02 00:50:31 -0400

    chore: release v1.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.adaryus.builds"
         minSdk = 26
         targetSdk = 34
-        versionCode = 121
-        versionName = "1.2.1"
+        versionCode = 122
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
